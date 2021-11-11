61e39cb8d577fbc31a1f25f3684e9d581ad98fca
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-11-11 18:20:13 +0300

    adds imports from flask_sqlalchemy and bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     # initializing flask extension
