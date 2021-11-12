e174a629ff8a55ec6000f1f3438ff7aa6dc71e81
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-11-12 13:15:49 +0300

    adds user models pitches and comments

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,23 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
-  # initializing flask extension
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
+login_manager =LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
+
+
 def create_app(config_name):
     app = Flask(__name__)
+    
+     # initializing flask extension
+    bootstrap.init_app(app)
+    db.init_app(app)
   
     
     # creating app configurations
