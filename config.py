de6ab2c190a0fac5e848da2cb8abd477ea5eb26b
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-11-14 20:29:43 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -1,36 +1,25 @@
 import os
 class Config:
-    """
-    parent configurations class
-    """
+    SECRET_KEY = 'brian'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitcheses'
-    SECRET_KEY = "brian"
-    
-    
-    MAIL_SERVER = 'smtp.googlemail.com'
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+    # email configurations
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-class Prodconfig(Config):
-    """
-    production cofiguration
-    """
-    
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
-
-class DevConfig(Config):
-    """
-    Development configuration
-    """
-    
+class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitcheses'
-    SECRET_KEY = "brian"
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitcheses'
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitcheses'
     DEBUG = True
-
-config_options={
-    'development':DevConfig,
-    'production':Prodconfig,
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
 }
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME= cheruiyotbrian9884@gmail.com
+export MAIL_PASSWORD= Akiche@9884
+
+python3 manage.py server
